8e463f0a06a5bb8a5d758e42da3faa390487e874
bmulla <bmulla@qatar.cmu.edu>
2019-04-13 13:38:47 +0000

    homepage added but lagging navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   resources :employees
   resources :stores
   resources :assignments
-  root 'employees#index'
+  # Semi-static page routes
+  get 'home' => 'home#home', as: :home
+  get 'about' => 'home#about', as: :about
+  get 'contact' => 'home#contact', as: :contact
+  get 'privacy' => 'home#privacy', as: :privacy
+  
+  # Set the root url
+  root :to => 'home#home'  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
