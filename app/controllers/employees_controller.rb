f9a4abf04622e641731cfcfde5ac3adf91a253f4
bmulla <bmulla@qatar.cmu.edu>
2019-04-17 18:54:13 +0000

    works

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -16,22 +16,12 @@ class EmployeesController < ApplicationController
   # GET /employees/new
   def new
     @employee = Employee.new
-    # echo params(user_attributes: [:email])
-    # if params(user_attributes: [:email]) == nil 
-    #   respond_to do |format|
-    #     if @employee.save
-    #       format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
-    #       format.json { render :show, status: :created, location: @employee }
-    #     end
-    #   end
-    # else
     @employee.build_user
-    # end
+
   end
 
   # GET /employees/1/edit
   def edit
-    @employee.build_user
   end
 
   # POST /employees
