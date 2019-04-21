b6119d4eaf30962c46fc3344732874b2a1d69e2b
bmulla <bmulla@qatar.cmu.edu>
2019-04-21 20:48:36 +0000

    made edits, user, assignment, and shift left

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   should belong_to(:employee)
+   should have_secure_password
+   should validate_uniqueness_of(:email)
+   
+  should allow_value("email@address.com").for(:email)
+  should_not allow_value("blos").for(:email)
+
 end
