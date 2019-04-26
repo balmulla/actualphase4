fed82f7c8bb289cb4bc747b00eb4ff09990b2ac1
bmulla <bmulla@qatar.cmu.edu>
2019-04-26 09:42:16 +0000

    small bugs

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -6,7 +6,11 @@ class DemosController < ApplicationController
         user = User.find_by(email: params[:demo][:email])
         if user && user.authenticate(params[:demo][:password])
             login(user)
-            redirect_to employees_path
+            # redirect_to employees_url+"/"+user.employee.id.to_s
+            respond_to do |format|
+                format.html { redirect_to employees_url+"/"+user.employee.id.to_s, notice: 'Welcome! '+ user.employee.proper_name }
+                format.json { head :no_content }
+            end
         else
             flash.now[:danger] = "Invalid email or password"
             render 'new'
